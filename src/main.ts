e6b94695bfb95746b22512d4d13e11c9decc46a2
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-08-25 15:41:27 +0200

    UPD - Bonus 2 done

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 const API_URL = 'http://localhost:3333';
-import type { Actress } from "./types";
+import type { Person, Actress, Actor } from "./types";
 
-const nationalities = [
+const actressNationalities = [
   'American',
   'British',
   'Astrualian',
@@ -15,7 +15,27 @@ const nationalities = [
   'Chinese'
 ];
 
-const isValid = (data: unknown): data is Actress => {
+const actorNationalities = [
+  'American',
+  'British',
+  'Astrualian',
+  'Israeli-American',
+  'South African',
+  'French',
+  'Indian',
+  'Israeli',
+  'Spanish',
+  'South Korean',
+  'Chinese',
+  'Scottish',
+  'New Zealand',
+  'Hong Kong',
+  'German',
+  'Canadian',
+  'Irish'
+];
+
+const isPersonValid = (data: unknown): data is Person => {
   return (
     data !== null &&
     typeof data === 'object' &&
@@ -30,7 +50,13 @@ const isValid = (data: unknown): data is Actress => {
     'biography' in data &&
     typeof data.biography === 'string' &&
     'image' in data &&
-    typeof data.image === 'string' &&
+    typeof data.image === 'string'
+  )
+}
+
+const isActressValid = (data: unknown): data is Actress => {
+  return (
+    isPersonValid(data) &&
     'most_famous_movies' in data &&
     data.most_famous_movies instanceof Array &&
     data.most_famous_movies.length === 3 &&
@@ -39,16 +65,33 @@ const isValid = (data: unknown): data is Actress => {
     typeof data.awards === 'string' &&
     'nationality' in data &&
     typeof data.nationality === 'string' &&
-    nationalities.includes(data.nationality)
+    actressNationalities.includes(data.nationality)
   )
 };
+
+const isActorValid = (data: unknown): data is Actor => {
+  return (
+    isPersonValid(data) &&
+    'know_for' in data &&
+    data.know_for instanceof Array &&
+    data.know_for.length === 3 &&
+    data.know_for.every(movie => typeof movie === 'string') &&
+    'awards' in data &&
+    data.awards instanceof Array &&
+    (data.awards.length === 1 || data.awards.length === 2) &&
+    data.awards.every(award => typeof award === 'string') &&
+    'nationality' in data &&
+    typeof data.nationality === 'string' &&
+    actorNationalities.includes(data.nationality)
+  )
+}
 
 const getActress = async (id: number): Promise<Actress | null> => {
   try {
     const res = await fetch(`${API_URL}/actresses/${id}`);
     if (!res.ok) throw new Error(`Errore ${res.status}: ${res.statusText}`);
     const data: unknown = await res.json();
-    if (!isValid(data)) throw new Error('Formato non valido');
+    if (!isActressValid(data)) throw new Error('Formato non valido');
     return data;
   } catch (e) {
     if (e instanceof Error) {
@@ -60,14 +103,49 @@ const getActress = async (id: number): Promise<Actress | null> => {
   }
 };
 
+const getActor = async (id: number): Promise<Actor | null> => {
+  try {
+    const res = await fetch(`${API_URL}/actors/${id}`);
+    if (!res.ok) throw new Error(`Errore ${res.status}: ${res.statusText}`);
+    const data: unknown = await res.json();
+    if (!isActorValid(data)) throw new Error('Formato non valido');
+    return data;
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error(`Errore nel recupero dei dati dell'attore:`, e);
+    } else {
+      console.error('Errore sconosciuto:', e);
+    }
+    return null
+  }
+}
+
 const getAllActress = async (): Promise<Actress[]> => {
   try {
     const res = await fetch(`${API_URL}/actresses`);
     if (!res.ok) throw new Error(`Errore ${res.status}: ${res.statusText}`);
     const data: unknown = res.json();
     if (!Array.isArray(data)) throw new Error(`Formato non valido`);
-    const validActress = data.filter(d => isValid(d));
+    const validActress = data.filter(d => isActressValid(d));
     return validActress;
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error(`Errore nel recupero dei dati delle attrici:`, e);
+    } else {
+      console.error('Errore sconosciuto:', e);
+    }
+    return [];
+  }
+}
+
+const getAllActors = async (): Promise<Actor[]> => {
+  try {
+    const res = await fetch(`${API_URL}/actors`);
+    if (!res.ok) throw new Error(`Errore ${res.status}: ${res.statusText}`);
+    const data: unknown = res.json();
+    if (!Array.isArray(data)) throw new Error(`Formato non valido`);
+    const validActors = data.filter(d => isActorValid(d));
+    return validActors;
   } catch (e) {
     if (e instanceof Error) {
       console.error(`Errore nel recupero dei dati delle attrici:`, e);
@@ -91,6 +169,19 @@ const getActresses = async (arr: number[]): Promise<(Actress | null)[]> => {
   }
 }
 
+const getActors = async (arr: number[]): Promise<(Actor | null)[]> => {
+  try {
+    return await Promise.all(arr.map(id => getActor(id)));
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error(`Errore nel recupero dei dati delle attrici:`, e);
+    } else {
+      console.error('Errore sconosciuto:', e);
+    }
+    return [];
+  }
+}
+
 const generateId = (): number => Math.floor(Math.random() * 99999);
 
 const createActress = (data: Omit<Actress, 'id'>): Actress => {
@@ -100,9 +191,23 @@ const createActress = (data: Omit<Actress, 'id'>): Actress => {
   }
 }
 
+const createActor = (data: Omit<Actor, 'id'>): Actor => {
+  return {
+    id: generateId(),
+    ...data
+  }
+}
+
 const updateActress = (actress: Actress, updates: Partial<Omit<Actress, 'id' | 'name'>>): Actress => {
   return {
     ...actress,
+    ...updates
+  }
+}
+
+const updateActor = (actor: Actor, updates: Partial<Omit<Actor, 'id' | 'name'>>): Actor => {
+  return {
+    ...actor,
     ...updates
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-type Person = {
+export type Person = {
     readonly id: number,
     readonly name: string,
     birth_year: number,
@@ -7,8 +7,36 @@ type Person = {
     image: string
 };
 
+type ActressNationality =
+    | 'American'
+    | 'British'
+    | 'Astrualian'
+    | 'Israeli-American'
+    | 'South African'
+    | 'French'
+    | 'Indian'
+    | 'Israeli'
+    | 'Spanish'
+    | 'South Korean'
+    | 'Chinese'
+
+type ActorNationality =
+    | ActressNationality
+    | 'Scottish'
+    | 'New Zealand'
+    | 'Hong Kong'
+    | 'German'
+    | 'Canadian'
+    | 'Irish'
+
 export type Actress = Person & {
     most_famous_movies: [string, string, string],
     awards: string,
-    nationality: 'American' | 'British' | 'Astrualian' | 'Israeli-American' | 'South African' | 'French' | 'Indian' | 'Israeli' | 'Spanish' | 'South Korean' | 'Chinese'
+    nationality: ActressNationality
 };
+
+export type Actor = Person & {
+    know_for: [string, string, string],
+    awards: [string] | [string, string],
+    nationality: ActorNationality
+}
